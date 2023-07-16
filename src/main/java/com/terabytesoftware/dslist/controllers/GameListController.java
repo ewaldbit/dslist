b9ccf8676b073c034f6971090fe516842799d2ac
Ewald Bitencourt <ewaldbit@gmail.com>
2023-07-16 17:12:46 -0300

    reposition game by list implemented

--- src/main/java/com/terabytesoftware/dslist/controllers/GameListController.java
+++ src/main/java/com/terabytesoftware/dslist/controllers/GameListController.java
@@ -2,13 +2,11 @@ package com.terabytesoftware.dslist.controllers;
 
 import com.terabytesoftware.dslist.dtos.GameListDTO;
 import com.terabytesoftware.dslist.dtos.GameMinDTO;
+import com.terabytesoftware.dslist.dtos.ReplacementDTO;
 import com.terabytesoftware.dslist.services.GameListService;
 import com.terabytesoftware.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,10 +26,15 @@ public class GameListController {
         return result;
     }
 
-    @GetMapping("/{listId}/games")
+    @GetMapping(value = "/{listId}/games")
     public List<GameMinDTO> findByList(@PathVariable Long listId){
         List<GameMinDTO> result = gameService.findByList(listId);
         return result;
+    }
+
+    @PostMapping(value = "/{listId}/replacement")
+    public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body) {
+        gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
     }
 
 
--- src/main/java/com/terabytesoftware/dslist/services/GameListService.java
+++ src/main/java/com/terabytesoftware/dslist/services/GameListService.java
@@ -2,7 +2,9 @@ package com.terabytesoftware.dslist.services;
 
 import com.terabytesoftware.dslist.dtos.GameListDTO;
 import com.terabytesoftware.dslist.entities.GameList;
+import com.terabytesoftware.dslist.projections.GameMinProjection;
 import com.terabytesoftware.dslist.repositories.GameListRepository;
+import com.terabytesoftware.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,11 +18,29 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
-    public List<GameListDTO> findAll(){
+    public List<GameListDTO> findAll() {
 
         List<GameList> result = gameListRepository.findAll();
         return result.stream().map(GameListDTO::new).collect(Collectors.toList());
+    }
 
+    @Transactional
+    public void move(Long listID, int sourceIndex, int destinationIndex) {
+
+        List<GameMinProjection> list = gameRepository.searchByList(listID);
+
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+        int min = (sourceIndex < destinationIndex) ? sourceIndex : destinationIndex;
+        int max = (sourceIndex < destinationIndex) ? destinationIndex : sourceIndex;
+
+        for (int i = min; i <= max; i++) {
+            gameListRepository.updateBelongingPosition(listID, list.get(i).getId(), i);
+        }
     }
 }
