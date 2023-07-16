91bbbc1e39103eba96ed4f4a2808d7a0fe3201f2
Ewald Bitencourt <ewaldbit@gmail.com>
2023-07-16 17:37:52 -0300

    edit annotation problems

--- src/main/java/com/terabytesoftware/dslist/controllers/GameListController.java
+++ src/main/java/com/terabytesoftware/dslist/controllers/GameListController.java
@@ -6,7 +6,11 @@ import com.terabytesoftware.dslist.dtos.ReplacementDTO;
 import com.terabytesoftware.dslist.services.GameListService;
 import com.terabytesoftware.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
 
